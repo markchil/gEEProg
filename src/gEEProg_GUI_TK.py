bbba80fa007a3b739817d5bbe3379b74e00e46ac
markchil <mark.chilenski@gmail.com>
2014-06-28 15:20:29 -0400

    Fix case.

--- src/gEEProg_GUI_TK.py
+++ src/gEEProg_GUI_TK.py
@@ -411,7 +411,7 @@ class GEEProgMainWindow(tk.Tk):
         
         # img = tk.PhotoImage(file='../graphics/Icon.ico')
         # self.tk.call('wm', 'iconbitmap', self._w, img)
-        self.iconbitmap('../graphics/icon.ico')
+        self.iconbitmap('../graphics/Icon.ico')
         # top = tk.Toplevel()
         # tk.Label(top, image=img).pack()
         # top.overrideredirect(1)
